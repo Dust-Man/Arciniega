104c473205233d6f4269952c679804a76539ef3b
Emmaradaway <ema63298@gmail.com>
2024-11-12 15:51:25 -0600

    Cards bien

--- productos_1.php
+++ productos_1.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./css/principal.css">
+    <script src="./scripts/header.js"></script>
 </head>
 
 <body>
@@ -16,7 +17,8 @@
         <div class="grid4x4">
             <?php
             include "./php/productos-sucursal-1/paginacion_1.php";
-            ?>
+            
+?>
             </div>
         </div>
        
