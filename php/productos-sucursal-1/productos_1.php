f4fbb2d4208efa8aed06e037bd5a1fc646459879
Emmaradaway <ema63298@gmail.com>
2024-11-12 09:05:50 -0600

    Cards

---
+++ php/productos-sucursal-1/productos_1.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/principal.css">
+</head>
+<body>
+    <?php
+    include "../../header.php";
+    ?>
+<div class="layout1 ">
+<div class="grid4x4">
+    <div class="card">
+    <a class="card1" href="#">
+        <p>This is heading</p>
+        <p class="small">Card description with lots of great facts and interesting details.</p>
+        <div class="go-corner" href="#">
+        <div class="go-arrow">
+            →
+        </div>
+        </div>
+    </a>
+    </div>
+    <div class="card">
+    <a class="card1" href="#">
+        <p>This is heading</p>
+        <p class="small">Card description with lots of great facts and interesting details.</p>
+        <div class="go-corner" href="#">
+        <div class="go-arrow">
+            →
+        </div>
+        </div>
+    </a>
+    </div>
+    <div class="card">
+    <a class="card1" href="#">
+        <p>This is heading</p>
+        <p class="small">Card description with lots of great facts and interesting details.</p>
+        <div class="go-corner" href="#">
+        <div class="go-arrow">
+            →
+        </div>
+        </div>
+    </a>
+    </div>
+    <div class="card">
+    <a class="card1" href="#">
+        <p>This is heading</p>
+        <p class="small">Card description with lots of great facts and interesting details.</p>
+        <div class="go-corner" href="#">
+        <div class="go-arrow">
+            →
+        </div>
+        </div>
+    </a>
+    </div>
+    <div class="card">
+    <a class="card1" href="#">
+        <p>This is heading</p>
+        <p class="small">Card description with lots of great facts and interesting details.</p>
+        <div class="go-corner" href="#">
+        <div class="go-arrow">
+            →
+        </div>
+        </div>
+    </a>
+    </div>
+    <div class="card">
+    <a class="card1" href="#">
+        <p>This is heading</p>
+        <p class="small">Card description with lots of great facts and interesting details.</p>
+        <div class="go-corner" href="#">
+        <div class="go-arrow">
+            →
+        </div>
+        </div>
+    </a>
+    </div>
+    <div class="card">
+    <a class="card1" href="#">
+        <p>This is heading</p>
+        <p class="small">Card description with lots of great facts and interesting details.</p>
+        <div class="go-corner" href="#">
+        <div class="go-arrow">
+            →
+        </div>
+        </div>
+    </a>
+    </div>
+    <div class="card">
+    <a class="card1" href="#">
+        <p>This is heading</p>
+        <p class="small">Card description with lots of great facts and interesting details.</p>
+        <div class="go-corner" href="#">
+        <div class="go-arrow">
+            →
+        </div>
+        </div>
+    </a>
+    </div>
+    <div class="card">
+    <a class="card1" href="#">
+        <p>This is heading</p>
+        <p class="small">Card description with lots of great facts and interesting details.</p>
+        <div class="go-corner" href="#">
+        <div class="go-arrow">
+            →
+        </div>
+        </div>
+    </a>
+    </div>
+</div>
+</div>
+<div class="layout1 --imagen-cemento-c-azul">
+
+</div>
+
+    
+</body>
+</html>
