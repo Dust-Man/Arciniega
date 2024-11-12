081caa50610aac0956f61a86e07d0a7d8a14a357
Dust-Man <carlosgaelhp@gmail.com>
2024-11-12 16:03:33 -0600

    arreglo seccion noticias

--- noticias.php
+++ noticias.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,8 +8,9 @@
     <link rel="stylesheet" href="./css/principal.css">
     <link rel="icon" href="./img/logos/logo.png" type="png/jpg">
 </head>
+
 <body>
-<?php
+    <?php
         include 'header.php';
         include 'php/conexion.php';
 
@@ -16,21 +18,32 @@
         $sql = "SELECT * FROM posts ORDER BY created_at DESC";
         $result = mysqli_query($conexion, $sql);
 ?>
-<main>
-    <div class="layout1 --color-dark --l1-nocentrar noticias">
-        <?php while ($post = mysqli_fetch_assoc($result)): ?>
-            <h2><?php echo htmlspecialchars($post['title']); ?></h2>
-            <p>
-                <?php
-                // Mostrar solo los primeros 100 caracteres del contenido
-                echo nl2br(htmlspecialchars(substr($post['content'], 0, 100))) . '...';
-                ?>
-            </p>
-            <p><em>Publicado el <?php echo $post['created_at']; ?></em></p>
-            <a href="post.php?id=<?php echo $post['id']; ?>">Leer más</a>
+    <main>
+        <div class="layout1 --color-dark  noticias">
+            <?php while ($post = mysqli_fetch_assoc($result)): ?>
+            <article class="noticias-entrada">
+                <div>
+                    <h2><?php echo htmlspecialchars($post['title']); ?></h2>
+                    <p>
+                        <?php
+                        // Mostrar solo los primeros 100 caracteres del contenido
+                        echo nl2br(htmlspecialchars(substr($post['content'], 0, 100))) . '...';
+                        ?>
+                    </p>
+
+                    <p><em>Publicado el <?php echo $post['created_at']; ?></em></p>
+                    <a href="post.php?id=<?php echo $post['id']; ?>">Leer más</a>
+                    
+                </div>
+                <div class="contenedor-imagen-entrada-noticia">
+                    <?php echo "<img class='imagen-entrada-noticia' src='".$post['banner_url']."'>" ?>
+                </div>
+
+            </article>
             <hr>
-        <?php endwhile; ?>
-    </div>
-</main>
+            <?php endwhile; ?>
+        </div>
+    </main>
 </body>
+
 </html>
