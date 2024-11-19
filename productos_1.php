ac53adb4a6439d84b0cd6b41355cc7ac6c240c09
Emmaradaway <ema63298@gmail.com>
2024-11-19 08:53:39 -0600

    Buscador bien okey ,no lo pude hacer mejor:(

--- productos_1.php
+++ productos_1.php
@@ -13,20 +13,35 @@
     <?php
     include "./header.php";
     ?>
-  <div class="grid4x4">
-  <?php
-            include "./php/productos-sucursal-1/paginacion_1.php";
-            
-?>
-  
-  </div>
-           
-          
-            
-       
+    <div class="grid4x4">
+    <form class="form">
+      
+          <svg width="17" height="16" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-labelledby="search">
+              <path d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9" stroke="currentColor" stroke-width="1.333" stroke-linecap="round" stroke-linejoin="round"></path>
+          </svg>
 
+      <input class="input" placeholder="Buscar algo..."  type="text" id="search-input">
+      <button class="reset" type="reset">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
+          </svg>
+      </button>
+  </form>
+  </div>
+  <div id="results">
+    <!-- Aqui se generaran los resultados xd -->
+</div>
+
+<div  class="pagination" id="pagination">
+    <!--Y aqui la paginacion  -->
+</div>
+
+
+ 
+          
 </body>
-<script src="./scripts/header.js"></script>
+<script src="./scripts/header.js" defer></script>
+<script src="./scripts/buscar.js" defer></script>
 
 
 </html>
